c5ae8b5645a872d0fd1b31572de1395c7945dfad
Renan Bandeira <email@renanbandeira.com.br>
2017-01-15 15:36:26 -0300

    Made changes

--- bargraphlib/src/main/java/br/com/renanbandeira/bargraphlib/view/BarGraph.java
+++ bargraphlib/src/main/java/br/com/renanbandeira/bargraphlib/view/BarGraph.java
@@ -110,6 +110,10 @@ public class BarGraph extends RecyclerView {
   }
 
   @Override public void setAdapter(Adapter adapter) {
+    if (isInEditMode()) {
+      super.setAdapter(adapter);
+      return;
+    }
     if (!(adapter instanceof BaseGraphBarAdapter)) {
       throw new UnsupportedClassVersionError("You should set a BaseGraphBarAdapter!");
     }
